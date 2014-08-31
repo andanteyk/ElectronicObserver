3385d9c9f5ae0d3ae3e92e8c306e1cf8fcf1d6a9
andanteyk <andante.soft@gmail.com>
2014-08-31 10:29:07 +0900

    First Commit

---
+++ ElectronicObserver/Form/FormMain.Designer.cs
@@ -0,0 +1,34 @@
+﻿namespace ElectronicObserver {
+	partial class FormMain {
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose( bool disposing ) {
+			if ( disposing && ( components != null ) ) {
+				components.Dispose();
+			}
+			base.Dispose( disposing );
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent() {
+			this.components = new System.ComponentModel.Container();
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Text = "Form1";
+		}
+
+		#endregion
+	}
+}
+
---
+++ ElectronicObserver/README.md
@@ -0,0 +1,13 @@
+﻿
+七四式電子観測儀
+================
+
+*開発中です*
+
+使用しているライブラリ
+----------------------
+
+* [DynamicJson](http://dynamicjson.codeplex.com/)
+* [DockPanel Suite](http://dockpanelsuite.com/)
+* [FiddlerCore](http://www.telerik.com/fiddler/fiddlercore)
+
