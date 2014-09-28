ca3bf4dc6d2e79de86aa48e3baef93f11f97ccf9
andanteyk <andante.soft@gmail.com>
2014-09-28 11:09:43 +0900

    レイアウト保存処理の変更
    以前の処理では最小化時にウィンドウが彼方に吹っ飛ぶ不具合があったので、正式なものを使って実装しなおした

--- ElectronicObserver/Window/Support/WindowPlacementManager.cs
+++ ElectronicObserver/Window/Support/WindowPlacementManager.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,8 +14,141 @@ namespace ElectronicObserver.Window.Support {
 
 	public static class WindowPlacementManager {
 
+		#region 各種宣言
+
+		[DllImport( "user32.dll" )]
+		public static extern bool SetWindowPlacement(
+			IntPtr hWnd,
+			[In] ref WINDOWPLACEMENT lpwndpl );
+
+		[DllImport( "user32.dll" )]
+		public static extern bool GetWindowPlacement(
+			IntPtr hWnd,
+			out WINDOWPLACEMENT lpwndpl );
+
+
+		[Serializable]
+		[StructLayout( LayoutKind.Sequential )]
+		public struct WINDOWPLACEMENT {
+			public int length;
+			public int flags;
+			public SW showCmd;
+			public POINT minPosition;
+			public POINT maxPosition;
+			public RECT normalPosition;
+		}
+
+		[Serializable]
+		[StructLayout( LayoutKind.Sequential )]
+		public struct POINT {
+			public int X;
+			public int Y;
+
+			public POINT( int x, int y ) {
+				this.X = x;
+				this.Y = y;
+			}
+		}
+
+		[Serializable]
+		[StructLayout( LayoutKind.Sequential )]
+		public struct RECT {
+			public int Left;
+			public int Top;
+			public int Right;
+			public int Bottom;
+
+			public RECT( int left, int top, int right, int bottom ) {
+				this.Left = left;
+				this.Top = top;
+				this.Right = right;
+				this.Bottom = bottom;
+			}
+		}
+
+		public enum SW {
+			HIDE = 0,
+			SHOWNORMAL = 1,
+			SHOWMINIMIZED = 2,
+			SHOWMAXIMIZED = 3,
+			SHOWNOACTIVATE = 4,
+			SHOW = 5,
+			MINIMIZE = 6,
+			SHOWMINNOACTIVE = 7,
+			SHOWNA = 8,
+			RESTORE = 9,
+			SHOWDEFAULT = 10,
+		}
+
+
+		public class WindowPlacementWrapper {
+
+			public WINDOWPLACEMENT RawData;
+
+			public int flags {
+				get { return RawData.flags; }
+				set { RawData.flags = value; }
+			}
+
+			public int showCmd {
+				get { return (int)RawData.showCmd; }
+				set { RawData.showCmd = (SW)value; }
+			}
+
+			public int minPositionX {
+				get { return RawData.minPosition.X; }
+				set { RawData.minPosition.X = value; }
+			}
+
+			public int minPositionY {
+				get { return RawData.minPosition.Y; }
+				set { RawData.minPosition.Y = value; }
+			}
+
+			public int maxPositionX {
+				get { return RawData.maxPosition.X; }
+				set { RawData.maxPosition.X = value; }
+			}
+
+			public int maxPositionY {
+				get { return RawData.maxPosition.Y; }
+				set { RawData.maxPosition.Y = value; }
+			}
+
+			public int normalPositionLeft {
+				get { return RawData.normalPosition.Left; }
+				set { RawData.normalPosition.Left = value; }
+			}
+
+			public int normalPositionTop {
+				get { return RawData.normalPosition.Top; }
+				set { RawData.normalPosition.Top = value; }
+			}
+
+			public int normalPositionRight {
+				get { return RawData.normalPosition.Right; }
+				set { RawData.normalPosition.Right = value; }
+			}
+
+			public int normalPositionBottom {
+				get { return RawData.normalPosition.Bottom; }
+				set { RawData.normalPosition.Bottom = value; }
+			}
+
+
+			public WindowPlacementWrapper() {
+				RawData = new WINDOWPLACEMENT();
+				RawData.length = Marshal.SizeOf( RawData );
+				RawData.flags = 0;
+			}
+
+		}
+		#endregion
+
+
+
 		public static string WindowPlacementConfigPath {
-			get { return @"Settings\WindowPlacement.cfg"; }
+			get { return @"Settings\WindowPlacement.json"; }
 		}
 
 
@@ -30,10 +164,13 @@ namespace ElectronicObserver.Window.Support {
 						settings = sr.ReadToEnd();
 					}
 
-					Rectangle rect = DynamicJson.Parse( settings );
 
-					form.Location = rect.Location;
-					form.ClientSize = rect.Size;
+					WindowPlacementWrapper wp = DynamicJson.Parse( settings );
+
+					if ( wp.RawData.showCmd == SW.SHOWMINIMIZED )
+						wp.RawData.showCmd = SW.SHOWNORMAL;
+
+					SetWindowPlacement( form.Handle, ref wp.RawData );
 
 				}
 
@@ -57,7 +194,13 @@ namespace ElectronicObserver.Window.Support {
 				}
 
 
-				string settings = DynamicJson.Serialize( new Rectangle( form.Location, form.ClientSize ) );
+				var wp = new WindowPlacementWrapper();
+				
+
+				GetWindowPlacement( form.Handle, out wp.RawData );
+
+
+				string settings = DynamicJson.Serialize( wp );
 
 				using ( StreamWriter sw = new StreamWriter( path ) ) {
 
@@ -72,15 +215,6 @@ namespace ElectronicObserver.Window.Support {
 			}
 		}
 
-
-		/*
-		public static void LoadSubWindowLayout( FormMain form, string path ) {
-
-
-
-		}
-		*/
-		
 
 	}
 
