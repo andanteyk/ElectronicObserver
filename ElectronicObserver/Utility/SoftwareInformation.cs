6d35faeb80a3cc635649f82fdb43a1820775490b
andanteyk <andante.soft@gmail.com>
2015-02-17 14:48:49 +0900

    Version 0.2.1
    ・一部の判定から退避艦を除外

--- ElectronicObserver/Utility/SoftwareInformation.cs
+++ ElectronicObserver/Utility/SoftwareInformation.cs
@@ -54,7 +54,7 @@ namespace ElectronicObserver.Utility {
 		/// </summary>
 		public static DateTime UpdateTime {
 			get {
-				return DateTimeHelper.CSVStringToTime( "2015/02/17 00:00:00" );
+				return DateTimeHelper.CSVStringToTime( "2015/02/17 14:00:00" );
 			}
 		}
 
