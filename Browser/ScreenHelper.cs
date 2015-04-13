783f4739f96e04233f7891238e1a4692afecd0f2
andanteyk <andante.soft@gmail.com>
2015-04-13 23:54:51 +0900

    フォーマット

--- Browser/ScreenHelper.cs
+++ Browser/ScreenHelper.cs
@@ -5,38 +5,35 @@ using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Browser
-{
-    static class ScreenHelper
-    {
-        [DllImport("user32.dll")]
-        static extern IntPtr GetDC(IntPtr ptr);
-        [DllImport("user32.dll")]
-        static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDc);
-        [DllImport("gdi32.dll")]
-        static extern uint GetDeviceCaps(
-        IntPtr hdc, // handle to DC
-        int nIndex // index of capability
-        );
-        [DllImport("user32.dll")]
-        static extern bool SetProcessDPIAware();
+namespace Browser {
+	static class ScreenHelper {
 
-        const int LOGPIXELSX = 88;
-        const int LOGPIXELSY = 90;
+		[DllImport( "user32.dll" )]
+		static extern IntPtr GetDC( IntPtr ptr );
+		[DllImport( "user32.dll" )]
+		static extern IntPtr ReleaseDC( IntPtr hWnd, IntPtr hDc );
+		[DllImport( "gdi32.dll" )]
+		static extern uint GetDeviceCaps(
+			IntPtr hdc, // handle to DC
+			int nIndex // index of capability
+		);
+		[DllImport( "user32.dll" )]
+		static extern bool SetProcessDPIAware();
 
-        static Dpi _dpi;
-        public static Dpi GetSystemDpi()
-        {
-            if (_dpi == null)
-            {
-                SetProcessDPIAware(); //重要
-                IntPtr screenDC = GetDC(IntPtr.Zero);
-                uint dpi_x = GetDeviceCaps(screenDC, LOGPIXELSX);
-                uint dpi_y = GetDeviceCaps(screenDC, LOGPIXELSY);
-                _dpi = new Dpi(dpi_x, dpi_y);
-                ReleaseDC(IntPtr.Zero, screenDC);
-            }
-            return _dpi;
-        }
-    }
+		const int LOGPIXELSX = 88;
+		const int LOGPIXELSY = 90;
+
+		static Dpi _dpi;
+		public static Dpi GetSystemDpi() {
+			if ( _dpi == null ) {
+				SetProcessDPIAware(); //重要
+				IntPtr screenDC = GetDC( IntPtr.Zero );
+				uint dpi_x = GetDeviceCaps( screenDC, LOGPIXELSX );
+				uint dpi_y = GetDeviceCaps( screenDC, LOGPIXELSY );
+				_dpi = new Dpi( dpi_x, dpi_y );
+				ReleaseDC( IntPtr.Zero, screenDC );
+			}
+			return _dpi;
+		}
+	}
 }
