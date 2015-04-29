137dae25893372f3eb0ccd8f4bd9a77f2292ff7e
andanteyk <andante.soft@gmail.com>
2015-04-29 20:06:29 +0900

    Version 1.2.2
    ・連合艦隊結成時に第1艦隊以外が出撃すると表示がおかしくなる不具合を修正

--- ElectronicObserver/Utility/SoftwareInformation.cs
+++ ElectronicObserver/Utility/SoftwareInformation.cs
@@ -54,7 +54,7 @@ namespace ElectronicObserver.Utility {
 		/// </summary>
 		public static DateTime UpdateTime {
 			get {
-				return DateTimeHelper.CSVStringToTime( "2015/04/29 12:00:00" );
+				return DateTimeHelper.CSVStringToTime( "2015/04/29 20:00:00" );
 			}
 		}
 
