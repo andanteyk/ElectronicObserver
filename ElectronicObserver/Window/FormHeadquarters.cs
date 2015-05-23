4fe892669da388046b581863fe7db5ce52700d26
Andante <andante.soft@gmail.com>
2015-05-23 23:52:07 +0900

    艦隊未解放で入渠するとエラーが出るのを修正
    ・グループタブのD&D入れ替えができなかった不具合を修正

--- ElectronicObserver/Window/FormHeadquarters.cs
+++ ElectronicObserver/Window/FormHeadquarters.cs
@@ -163,24 +163,26 @@ namespace ElectronicObserver.Window {
 
 			//Fleet
 			FlowPanelFleet.SuspendLayout();
-			int dropShips = 0;
-			int dropEqps = 0;
-			if ( db.Battle != null ) {
-				dropShips = db.Battle.DroppedShipCount;
-				dropEqps = db.Battle.DroppedEquipmentCount;
+			{
+				int dropShips = 0;
+				int dropEqps = 0;
+				if ( db.Battle != null ) {
+					dropShips = db.Battle.DroppedShipCount;
+					dropEqps = db.Battle.DroppedEquipmentCount;
+				}
+
+				ShipCount.Text = string.Format( "{0}/{1}", db.Ships.Count + dropShips, db.Admiral.MaxShipCount );
+				if ( db.Ships.Count > db.Admiral.MaxShipCount - 5 )
+					ShipCount.BackColor = Color.LightCoral;
+				else
+					ShipCount.BackColor = Color.Transparent;
+
+				EquipmentCount.Text = string.Format( "{0}/{1}", db.Equipments.Count + dropEqps, db.Admiral.MaxEquipmentCount );
+				if ( db.Equipments.Count > db.Admiral.MaxEquipmentCount + 3 - 20 )
+					EquipmentCount.BackColor = Color.LightCoral;
+				else
+					EquipmentCount.BackColor = Color.Transparent;
 			}
-
-			ShipCount.Text = string.Format( "{0}/{1}", db.Ships.Count + dropShips, db.Admiral.MaxShipCount );
-			if ( db.Ships.Count > db.Admiral.MaxShipCount - 5 )
-				ShipCount.BackColor = Color.LightCoral;
-			else
-				ShipCount.BackColor = Color.Transparent;
-
-			EquipmentCount.Text = string.Format( "{0}/{1}", db.Equipments.Count + dropEqps, db.Admiral.MaxEquipmentCount );
-			if ( db.Equipments.Count > db.Admiral.MaxEquipmentCount + 3 - 20 )
-				EquipmentCount.BackColor = Color.LightCoral;
-			else
-				EquipmentCount.BackColor = Color.Transparent;
 			FlowPanelFleet.ResumeLayout();
 
 			//UseItems
