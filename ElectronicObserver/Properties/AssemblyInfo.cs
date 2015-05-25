0e3e59b072b8da35e3002315ed64e3b2723d6c8b
tsanie <tsanie.lily@gmail.com>
2015-05-25 08:55:06 +0800

    version change

--- ElectronicObserver/Properties/AssemblyInfo.cs
+++ ElectronicObserver/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion( "1.206.524.1100" )]
-[assembly: AssemblyFileVersion( "1.206.524.1100" )]
+[assembly: AssemblyVersion( "1.206.525.0853" )]
+[assembly: AssemblyFileVersion( "1.206.525.0853" )]
--- ElectronicObserver/Utility/SoftwareInformation.cs
+++ ElectronicObserver/Utility/SoftwareInformation.cs
@@ -63,7 +63,7 @@ namespace ElectronicObserver.Utility {
 		/// 魔改版本
 		/// </summary>
 		public static double MakaiVersion {
-			get { return 524.1100; }
+			get { return 525.0853; }
 		}
 
 
