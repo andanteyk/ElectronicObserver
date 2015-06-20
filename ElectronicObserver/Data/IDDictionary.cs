e32430c86aeb062d9dce5b7a8524a47086c45e54
tsanie <tsorgy@gmail.com>
2015-06-20 10:34:16 +0800

    IDDictionary change

--- ElectronicObserver/Data/IDDictionary.cs
+++ ElectronicObserver/Data/IDDictionary.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,38 +11,76 @@ namespace ElectronicObserver.Data {
 	/// IDを持つデータのリストを保持します。
 	/// </summary>
 	/// <typeparam name="TData"></typeparam>
-	public class IDDictionary<TData> : Dictionary<int, TData> where TData : class, IIdentifiable {
+	public class IDDictionary<TData> : IEnumerable<KeyValuePair<int, TData>>, IEnumerable where TData : class, IIdentifiable {
+
+		private readonly IDictionary<int, TData> dict;
 
 		public IDDictionary() 
 			: this( new List<TData>() ){
 		}
 
-		public IDDictionary( IEnumerable<TData> source )
-			: base( source.ToDictionary( x => x.ID ) ) {
+		public IDDictionary( IEnumerable<TData> source ) {
+			dict = source.ToDictionary( x => x.ID );
 		}
 
 
 		internal void Add( TData data ) {
-			Add( data.ID, data );
+			dict.Add( data.ID, data );
 		}
 
 		internal void Remove( TData data ) {
-			Remove( data.ID );
+			dict.Remove( data.ID );
+		}
+
+		internal void Remove( int id ) {
+			dict.Remove( id );
 		}
 
 		internal int RemoveAll( Predicate<TData> predicate ) {
-			var removekeys = Values.Where( elem => predicate( elem ) ).Select( elem => elem.ID ).ToArray();
+			var removekeys = dict.Values.Where( elem => predicate( elem ) ).Select( elem => elem.ID ).ToArray();
 
 			foreach ( var key in removekeys ) {
-				Remove( key );
+				dict.Remove( key );
 			}
 
 			return removekeys.Count();
 		}
 
+		internal void Clear() {
+			dict.Clear();
+		}
 
-		public new TData this[int key] {
-			get { return ContainsKey( key ) ? base[key] : null; }
+
+		public bool ContainsKey( int key ) {
+			return dict.ContainsKey( key );
+		}
+
+		public IEnumerable<int> Keys {
+			get { return dict.Keys; }
+		}
+
+		public bool TryGetValue( int key, out TData value ) {
+			return dict.TryGetValue( key, out value );
+		}
+
+		public IEnumerable<TData> Values {
+			get { return dict.Values; }
+		}
+
+		public TData this[int key] {
+			get { return dict.ContainsKey( key ) ? dict[key] : null; }
+		}
+
+		public int Count {
+			get { return dict.Count; }
+		}
+
+		public IEnumerator<KeyValuePair<int, TData>> GetEnumerator() {
+			return dict.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator() {
+			return dict.GetEnumerator();
 		}
 	}
 
