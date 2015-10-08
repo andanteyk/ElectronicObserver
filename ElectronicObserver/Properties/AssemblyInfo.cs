8189035c07cbd46248e920c0f71251f9eae169e1
tsanie <tsorgy@gmail.com>
2015-10-08 10:16:41 +0800

    version up

--- ElectronicObserver/Properties/AssemblyInfo.cs
+++ ElectronicObserver/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Runtime.InteropServices;
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.8.0.0")]
-[assembly: AssemblyFileVersion("1.8.0.0")]
-[assembly: AssemblyInformationalVersion("1.8.0.0.net40")]
+[assembly: AssemblyFileVersion("1.8.2.0")]
+[assembly: AssemblyInformationalVersion("1.8.2.0.net40")]
