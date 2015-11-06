ee9f63a3e756444b10d86875724a8752ee1fd32d
HeriX <herix001@hotmail.com>
2015-11-06 08:54:05 +0800

    修正五航改二经验值计算可能引起的假死问题

--- ElectronicObserver/Window/FormFleet.cs
+++ ElectronicObserver/Window/FormFleet.cs
@@ -422,7 +422,11 @@ namespace ElectronicObserver.Window {
 							{
 								int level = ship_m.RemodelAfterLevel;
 								if ( ship.Level < level )
+								{
 									tip.AppendFormat( "改装まで: Lv. {0} / {1} exp.\n", level - ship.Level, Math.Max( ExpTable.ShipExp[level].Total - ship.ExpTotal, 0 ) );
+								
+									break;
+								}
 
 								ship_m = ship_m.RemodelAfterShip;
 							}
