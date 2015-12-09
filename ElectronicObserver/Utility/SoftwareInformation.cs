a09f4e4324f6f2b0656b68987c99e116431cedba
Andante <andante.soft@gmail.com>
2015-12-10 00:40:01 +0900

    Version 2.0.3
    * パラメータレコードの更新

--- ElectronicObserver/Utility/SoftwareInformation.cs
+++ ElectronicObserver/Utility/SoftwareInformation.cs
@@ -35,7 +35,7 @@ namespace ElectronicObserver.Utility {
 		/// </summary>
 		public static string VersionJapanese {
 			get {
-				return SoftwareNameJapanese + "二〇型改二乙";
+				return SoftwareNameJapanese + "二〇型改三";
 			}
 		}
 
@@ -44,7 +44,7 @@ namespace ElectronicObserver.Utility {
 		/// </summary>
 		public static string VersionEnglish {
 			get {
-				return "2.0.2.2";
+				return "2.0.3";
 			}
 		}
 
@@ -54,7 +54,7 @@ namespace ElectronicObserver.Utility {
 		/// </summary>
 		public static DateTime UpdateTime {
 			get {
-				return DateTimeHelper.CSVStringToTime( "2015/12/06 22:00:00" );
+				return DateTimeHelper.CSVStringToTime( "2015/12/10 01:00:00" );
 			}
 		}
 
