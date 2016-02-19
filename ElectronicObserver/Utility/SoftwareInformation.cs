22008f1d08437a529c7c042ee05210f8bc44a5d3
Andante <andante.soft@gmail.com>
2016-02-19 22:41:25 +0900

    Version 2.1.5

--- ElectronicObserver/Utility/SoftwareInformation.cs
+++ ElectronicObserver/Utility/SoftwareInformation.cs
@@ -35,7 +35,7 @@ namespace ElectronicObserver.Utility {
 		/// </summary>
 		public static string VersionJapanese {
 			get {
-				return SoftwareNameJapanese + "二一型改四";
+				return SoftwareNameJapanese + "二一型改五";
 			}
 		}
 
@@ -44,7 +44,7 @@ namespace ElectronicObserver.Utility {
 		/// </summary>
 		public static string VersionEnglish {
 			get {
-				return "2.1.4";
+				return "2.1.5";
 			}
 		}
 
@@ -54,7 +54,7 @@ namespace ElectronicObserver.Utility {
 		/// </summary>
 		public static DateTime UpdateTime {
 			get {
-				return DateTimeHelper.CSVStringToTime( "2016/02/11 02:00:00" );
+				return DateTimeHelper.CSVStringToTime( "2016/02/19 22:00:00" );
 			}
 		}
 
