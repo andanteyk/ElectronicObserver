3daa9220ff84c5facfb4d811d7763d0fe036b986
Andante <andante.soft@gmail.com>
2016-02-21 02:00:47 +0900

    フォーマット

--- ElectronicObserver/Window/FormBrowserHost.cs
+++ ElectronicObserver/Window/FormBrowserHost.cs
@@ -300,19 +300,19 @@ namespace ElectronicObserver.Window {
 		private string BuildDownstreamProxy() {
 			var config = Utility.Configuration.Config.Connection;
 
-            if ( !string.IsNullOrEmpty( config.DownstreamProxy ) ) {
-                return config.DownstreamProxy;
+			if ( !string.IsNullOrEmpty( config.DownstreamProxy ) ) {
+				return config.DownstreamProxy;
 
-            } else if (config.UseSystemProxy) {
-                return APIObserver.Instance.ProxyPort.ToString();
+			} else if ( config.UseSystemProxy ) {
+				return APIObserver.Instance.ProxyPort.ToString();
 
-            } else if (config.UseUpstreamProxy) {
-                return string.Format(
-                    "http=127.0.0.1:{0};https={1}:{2}",
-                    APIObserver.Instance.ProxyPort,
-                    config.UpstreamProxyAddress,
-                    config.UpstreamProxyPort);
-            } else {
+			} else if ( config.UseUpstreamProxy ) {
+				return string.Format(
+					"http=127.0.0.1:{0};https={1}:{2}",
+					APIObserver.Instance.ProxyPort,
+					config.UpstreamProxyAddress,
+					config.UpstreamProxyPort );
+			} else {
 				return string.Format( "http=127.0.0.1:{0}", APIObserver.Instance.ProxyPort );
 			}
 		}
