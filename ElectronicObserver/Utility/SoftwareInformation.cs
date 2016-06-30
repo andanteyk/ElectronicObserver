ef5b7101bf655c3467b3c82e8304f1915f1268da
Andante <andante.soft@gmail.com>
2016-07-01 00:24:01 +0900

    Version 2.3.2
    * ブラウザ：右端に広告が出るのを阻止
    * 図鑑：艦船図鑑のソート処理を改善

--- ElectronicObserver/Utility/SoftwareInformation.cs
+++ ElectronicObserver/Utility/SoftwareInformation.cs
@@ -35,7 +35,7 @@ namespace ElectronicObserver.Utility {
 		/// </summary>
 		public static string VersionJapanese {
 			get {
-				return SoftwareNameJapanese + "二三型改";
+				return SoftwareNameJapanese + "二三型改二";
 			}
 		}
 
@@ -44,7 +44,7 @@ namespace ElectronicObserver.Utility {
 		/// </summary>
 		public static string VersionEnglish {
 			get {
-				return "2.3.1";
+				return "2.3.2";
 			}
 		}
 
@@ -54,7 +54,7 @@ namespace ElectronicObserver.Utility {
 		/// </summary>
 		public static DateTime UpdateTime {
 			get {
-				return DateTimeHelper.CSVStringToTime( "2016/06/10 23:00:00" );
+				return DateTimeHelper.CSVStringToTime( "2016/07/01 00:00:00" );
 			}
 		}
 
