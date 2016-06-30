9d1e9487f41dcab0e18098208627223f310d7b99
Andante <andante.soft@gmail.com>
2016-06-30 22:05:25 +0900

    2016/06/30 アップデートへの対応
    * 戦闘：先制対潜攻撃への対応
    * 図鑑：装備レアリティの追加への対応

---
+++ ElectronicObserver/Data/Battle/Phase/PhaseOpeningASW.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicObserver.Data.Battle.Phase {
+
+	/// <summary>
+	/// 開幕対潜攻撃フェーズの処理を行います。
+	/// </summary>
+	public class PhaseOpeningASW : PhaseShelling {
+
+		// 砲撃戦とフォーマットが同じなので流用
+
+		public PhaseOpeningASW( BattleData data, bool isEscort  )
+			: base( data, 0, "", isEscort ) {
+
+		}
+
+		public override bool IsAvailable {
+			get { return (int)RawData.api_opening_taisen_flag != 0; }
+		}
+
+		public override dynamic ShellingData {
+			get { return RawData.api_opening_taisen; }
+		}
+
+	}
+}
--- ElectronicObserver/Data/Constants.cs
+++ ElectronicObserver/Data/Constants.cs
@@ -90,7 +90,9 @@ namespace ElectronicObserver.Data {
 				case 4:
 					return "SSホロ";
 				case 5:
-					return "EXホロ";
+					return "SSホロ'";
+				case 6:
+					return "SSホロ+";
 				default:
 					return "不明";
 			}
@@ -104,14 +106,16 @@ namespace ElectronicObserver.Data {
 				case 0:
 					return 1;
 				case 1:
-					return 4;
+					return 3;
 				case 2:
-					return 5;
+					return 4;
 				case 3:
-					return 6;
+					return 5;
 				case 4:
-					return 7;
+					return 6;
 				case 5:
+					return 7;
+				case 6:
 					return 8;
 				default:
 					return 0;
