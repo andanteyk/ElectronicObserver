6a9ead948733f6207856dee5bc3a463b54ec97d8
CNA-Bld <cna.bld@gmail.com>
2017-03-02 20:46:44 +0800

    Warn user in FIPS enabled environment

--- ElectronicObserver/Program.cs
+++ ElectronicObserver/Program.cs
@@ -1,7 +1,10 @@
 ﻿using ElectronicObserver.Window;
+using ElectronicObserver.Utility;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -18,6 +21,28 @@ namespace ElectronicObserver {
 			if ( !mutex.WaitOne( 0, false ) ) {
 				// 多重起動禁止
 				MessageBox.Show( "既に起動しています。\r\n多重起動はできません。", "七四式電子観測儀", MessageBoxButtons.OK, MessageBoxIcon.Error );
+				return;
+			}
+
+			try
+			{
+				MD5.Create();
+			}
+			catch (TargetInvocationException ex)
+			{
+				if (ex.InnerException is InvalidOperationException)
+				{
+					MessageBox.Show(
+						"FIPS 有効の場合、このソフトは実行できません。\nレジストリの " +
+						"HKLM\\System\\CurrentControlSet\\Control\\Lsa\\FIPSAlgorithmPolicy\\Enabled" +
+						" キーの値を 0 に直してください。", "七四式電子観測儀", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
+				else
+				{
+					MessageBox.Show("MD5 ハッシュを作成できません。このエラーを報告してください。", 
+						"七四式電子観測儀", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				}
+				ErrorReporter.SendErrorReport(ex.InnerException, "MD5 ハッシュを作成できません。");
 				return;
 			}
 
