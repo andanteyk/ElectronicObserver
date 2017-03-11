f6b145a02a861874f3dbb7cf1aa2dd1e438426a9
CNA-Bld <cna.bld@gmail.com>
2017-03-11 20:59:34 +0800

    Cast IntPtr to long before casting it to uint

--- ElectronicObserver/Window/Integrate/FormIntegrated.cs
+++ ElectronicObserver/Window/Integrate/FormIntegrated.cs
@@ -262,7 +262,7 @@ namespace ElectronicObserver.Window.Integrate {
 				StripMenu_Detach.Enabled = true;
 			}
 
-			origStyle = (uint)WinAPI.GetWindowLong( hWnd, WinAPI.GWL_STYLE );
+			origStyle = unchecked((uint)(long)WinAPI.GetWindowLong( hWnd, WinAPI.GWL_STYLE ));
 			origOwner = WinAPI.GetWindowLong( hWnd, WinAPI.GWL_HWNDPARENT );
 
 			// ターゲットが最大化されていたら戻す
