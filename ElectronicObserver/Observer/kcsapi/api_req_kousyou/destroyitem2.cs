94850eded9e5888b7531916919eba06ff6daf422
RadarNyan <RadarNyan@moe9th.com>
2017-07-03 08:43:36 +0900

    Merge Duplicate DestroyItem Log

--- ElectronicObserver/Observer/kcsapi/api_req_kousyou/destroyitem2.cs
+++ ElectronicObserver/Observer/kcsapi/api_req_kousyou/destroyitem2.cs
@@ -17,12 +17,19 @@ namespace ElectronicObserver.Observer.kcsapi.api_req_kousyou {
 			// 削除処理が終わってからだと装備データが取れないため
 			db.QuestProgress.EquipmentDiscarded( APIName, data );
 
+			Dictionary<string, int> itemsDestroyed = new Dictionary<string, int>();
 
 			foreach ( string sid in data["api_slotitem_ids"].Split( ",".ToCharArray() ) ) {
-
 				int id = int.Parse( sid );
-				Utility.Logger.Add( 2, KCDatabase.Instance.Equipments[id].NameWithLevel + " を廃棄しました。" );
+				string name = KCDatabase.Instance.Equipments[id].NameWithLevel;
+				int amount;
+				itemsDestroyed.TryGetValue( name, out amount );
+				itemsDestroyed[name] = amount + 1;
 				db.Equipments.Remove( id );
+			}
+
+			foreach ( var item in itemsDestroyed ) {
+				Utility.Logger.Add( 2, String.Format( "{0}{1} を廃棄しました。", item.Key, item.Value > 1 ? " x " + item.Value : "" ) );
 			}
 			
 			base.OnRequestReceived( data );
