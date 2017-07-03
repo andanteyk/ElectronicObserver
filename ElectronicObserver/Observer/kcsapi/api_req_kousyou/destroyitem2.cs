21ba67e945144fd79219b24e743b51aac9964c33
Andante <andante.soft@gmail.com>
2017-07-03 23:54:49 +0900

    フォーマット
    * ついでに処理を調整

--- ElectronicObserver/Observer/kcsapi/api_req_kousyou/destroyitem2.cs
+++ ElectronicObserver/Observer/kcsapi/api_req_kousyou/destroyitem2.cs
@@ -11,7 +11,7 @@ namespace ElectronicObserver.Observer.kcsapi.api_req_kousyou {
 
 
 		public override void OnRequestReceived( Dictionary<string, string> data ) {
-			
+
 			KCDatabase db = KCDatabase.Instance;
 
 			// 削除処理が終わってからだと装備データが取れないため
@@ -19,19 +19,19 @@ namespace ElectronicObserver.Observer.kcsapi.api_req_kousyou {
 
 			Dictionary<string, int> itemsDestroyed = new Dictionary<string, int>();
 
-			foreach ( string sid in data["api_slotitem_ids"].Split( ",".ToCharArray() ) ) {
-				int id = int.Parse( sid );
+			foreach ( int id in data["api_slotitem_ids"].Split( ",".ToCharArray() ).Select( str => int.Parse( str ) ) ) {
 				string name = KCDatabase.Instance.Equipments[id].NameWithLevel;
 				int amount;
 				itemsDestroyed.TryGetValue( name, out amount );
 				itemsDestroyed[name] = amount + 1;
+
 				db.Equipments.Remove( id );
 			}
 
 			foreach ( var item in itemsDestroyed ) {
 				Utility.Logger.Add( 2, String.Format( "{0}{1} を廃棄しました。", item.Key, item.Value > 1 ? " x " + item.Value : "" ) );
 			}
-			
+
 			base.OnRequestReceived( data );
 		}
 
