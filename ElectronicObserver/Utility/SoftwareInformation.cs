8501f0dd8000e80d396c4bf8e1fe0555ba57d02a
Andante <andante.soft@gmail.com>
2017-11-22 00:34:13 +0900

    Version 3.0.2

--- ElectronicObserver/Utility/SoftwareInformation.cs
+++ ElectronicObserver/Utility/SoftwareInformation.cs
@@ -29,20 +29,20 @@ namespace ElectronicObserver.Utility
 		/// <summary>
 		/// バージョン(日本語, ソフトウェア名を含みます)
 		/// </summary>
-		public static string VersionJapanese => SoftwareNameJapanese + "三〇型改";
+		public static string VersionJapanese => SoftwareNameJapanese + "三〇型改二";
 
 
 		/// <summary>
 		/// バージョン(英語)
 		/// </summary>
-		public static string VersionEnglish => "3.0.1";
+		public static string VersionEnglish => "3.0.2";
 
 
 
 		/// <summary>
 		/// 更新日時
 		/// </summary>
-		public static DateTime UpdateTime => DateTimeHelper.CSVStringToTime("2017/11/21 02:00:00");
+		public static DateTime UpdateTime => DateTimeHelper.CSVStringToTime("2017/11/22 00:00:00");
 
 
 
