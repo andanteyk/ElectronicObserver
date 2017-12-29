db7569c72228bb0d0906480a9995d25bacf6cfda
Andante <andanteyk@gmail.com>
2017-12-30 07:22:09 +0900

    微修正

--- ElectronicObserver/Data/Battle/Detail/BattleDetail.cs
+++ ElectronicObserver/Data/Battle/Detail/BattleDetail.cs
@@ -149,11 +149,9 @@ namespace ElectronicObserver.Data.Battle.Detail
 
 			StringBuilder builder = new StringBuilder();
 
-			// 航空戦・支援攻撃時 AttackerIndex = BattleIndex.Invalid 、それで AttackerIndex.Side = BattleSides.FriendMain になる
-			// DefenderIndex.Side で判断すれば航空戦も正確に識別できるが表示が変になるので、最終的には AttackerIndex.Side で判断しだ
-			bool attackerIsFriend = AttackerIndex.Side == BattleSides.FriendMain || AttackerIndex.Side == BattleSides.FriendEscort;
-
-			builder.AppendFormat(attackerIsFriend ? "{0} → {1}\r\n" : "{1} ← {0}\r\n", GetAttackerName(), GetDefenderName());
+            // 航空戦・支援攻撃時 AttackerIndex = BattleIndex.Invalid 、それで AttackerIndex.Side = BattleSides.FriendMain になる
+            // DefenderIndex.Side で判断すれば航空戦も正確に識別できるが表示が変になるので、最終的には AttackerIndex.Side で判断した
+            builder.AppendFormat(AttackerIndex.IsFriend ? "{0} → {1}\r\n" : "{1} ← {0}\r\n", GetAttackerName(), GetDefenderName());
 
 
 			if (AttackType >= 0)
