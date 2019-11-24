f8bb6f53b0a02fce6c44a352185514a37f951d2d
Andante <andante.soft@gmail.com>
2019-11-24 22:59:47 +0900

    Version 4.3.1

--- ElectronicObserver/Utility/SoftwareInformation.cs
+++ ElectronicObserver/Utility/SoftwareInformation.cs
@@ -29,20 +29,20 @@ namespace ElectronicObserver.Utility
 		/// <summary>
 		/// バージョン(日本語, ソフトウェア名を含みます)
 		/// </summary>
-		public static string VersionJapanese => SoftwareNameJapanese + "四三型";
+		public static string VersionJapanese => SoftwareNameJapanese + "四三型改";
 
 
 		/// <summary>
 		/// バージョン(英語)
 		/// </summary>
-		public static string VersionEnglish => "4.3.0";
+		public static string VersionEnglish => "4.3.1";
 
 
 
 		/// <summary>
 		/// 更新日時
 		/// </summary>
-		public static DateTime UpdateTime => DateTimeHelper.CSVStringToTime("2019/10/01 01:00:00");
+		public static DateTime UpdateTime => DateTimeHelper.CSVStringToTime("2019/11/24 23:00:00");
 
 
 
