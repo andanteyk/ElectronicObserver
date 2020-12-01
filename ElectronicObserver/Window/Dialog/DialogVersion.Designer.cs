fb6f23fb00a406b2330dbc283ce7db16233da394
Mochizuki_Mk19 <Tsukumoyarei@gmail.com>
2020-12-01 20:06:48 +0100

    Small fix to version appearance

--- ElectronicObserver/Window/Dialog/DialogVersion.Designer.cs
+++ ElectronicObserver/Window/Dialog/DialogVersion.Designer.cs
@@ -28,100 +28,126 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			this.TextVersion = new System.Windows.Forms.Label();
-			this.label1 = new System.Windows.Forms.Label();
-			this.TextAuthor = new System.Windows.Forms.LinkLabel();
-			this.ButtonClose = new System.Windows.Forms.Button();
-			this.TextInformation = new System.Windows.Forms.LinkLabel();
-			this.label2 = new System.Windows.Forms.Label();
-			this.SuspendLayout();
-			// 
-			// TextVersion
-			// 
-			this.TextVersion.AutoSize = true;
-			this.TextVersion.Location = new System.Drawing.Point(12, 9);
-			this.TextVersion.Margin = new System.Windows.Forms.Padding(3);
-			this.TextVersion.Name = "TextVersion";
-			this.TextVersion.Size = new System.Drawing.Size(208, 15);
-			this.TextVersion.TabIndex = 1;
-			this.TextVersion.Text = "試製七四式電子観測儀零型 (ver. 0.0)";
-			// 
-			// label1
-			// 
-			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(12, 30);
-			this.label1.Margin = new System.Windows.Forms.Padding(3);
-			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(43, 15);
-			this.label1.TabIndex = 2;
-			this.label1.Text = "開発：";
-			// 
-			// TextAuthor
-			// 
-			this.TextAuthor.AutoSize = true;
-			this.TextAuthor.Location = new System.Drawing.Point(61, 30);
-			this.TextAuthor.Name = "TextAuthor";
-			this.TextAuthor.Size = new System.Drawing.Size(55, 15);
-			this.TextAuthor.TabIndex = 3;
-			this.TextAuthor.TabStop = true;
-			this.TextAuthor.Text = "Andante";
-			this.TextAuthor.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.TextAuthor_LinkClicked);
-			// 
-			// ButtonClose
-			// 
-			this.ButtonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-			this.ButtonClose.Location = new System.Drawing.Point(377, 127);
-			this.ButtonClose.Name = "ButtonClose";
-			this.ButtonClose.Size = new System.Drawing.Size(75, 23);
-			this.ButtonClose.TabIndex = 0;
-			this.ButtonClose.Text = "閉じる";
-			this.ButtonClose.UseVisualStyleBackColor = true;
-			this.ButtonClose.Click += new System.EventHandler(this.ButtonClose_Click);
-			// 
-			// TextInformation
-			// 
-			this.TextInformation.AutoSize = true;
-			this.TextInformation.Location = new System.Drawing.Point(12, 72);
-			this.TextInformation.Name = "TextInformation";
-			this.TextInformation.Size = new System.Drawing.Size(235, 15);
-			this.TextInformation.TabIndex = 5;
-			this.TextInformation.TabStop = true;
-			this.TextInformation.Text = "http://electronicobserver.blog.fc2.com/";
-			this.TextInformation.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.TextInformation_LinkClicked);
-			// 
-			// label2
-			// 
-			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(12, 51);
-			this.label2.Margin = new System.Windows.Forms.Padding(3);
-			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(200, 15);
-			this.label2.TabIndex = 4;
-			this.label2.Text = "配布元(不具合報告・連絡はこちらへ)：";
-			// 
-			// DialogVersion
-			// 
-			this.AcceptButton = this.ButtonClose;
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
-			this.ClientSize = new System.Drawing.Size(464, 162);
-			this.Controls.Add(this.label2);
-			this.Controls.Add(this.TextInformation);
-			this.Controls.Add(this.ButtonClose);
-			this.Controls.Add(this.TextAuthor);
-			this.Controls.Add(this.label1);
-			this.Controls.Add(this.TextVersion);
-			this.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
-			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-			this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-			this.MaximizeBox = false;
-			this.MinimizeBox = false;
-			this.Name = "DialogVersion";
-			this.ShowInTaskbar = false;
-			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
-			this.Text = "バージョン";
-			this.Load += new System.EventHandler(this.DialogVersion_Load);
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.TextVersion = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TextAuthor = new System.Windows.Forms.LinkLabel();
+            this.ButtonClose = new System.Windows.Forms.Button();
+            this.TextInformation = new System.Windows.Forms.LinkLabel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // TextVersion
+            // 
+            this.TextVersion.AutoSize = true;
+            this.TextVersion.Location = new System.Drawing.Point(12, 9);
+            this.TextVersion.Margin = new System.Windows.Forms.Padding(3);
+            this.TextVersion.Name = "TextVersion";
+            this.TextVersion.Size = new System.Drawing.Size(208, 15);
+            this.TextVersion.TabIndex = 1;
+            this.TextVersion.Text = "試製七四式電子観測儀零型 (ver. 0.0)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Margin = new System.Windows.Forms.Padding(3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "開発：";
+            // 
+            // TextAuthor
+            // 
+            this.TextAuthor.AutoSize = true;
+            this.TextAuthor.Location = new System.Drawing.Point(61, 30);
+            this.TextAuthor.Name = "TextAuthor";
+            this.TextAuthor.Size = new System.Drawing.Size(55, 15);
+            this.TextAuthor.TabIndex = 3;
+            this.TextAuthor.TabStop = true;
+            this.TextAuthor.Text = "Andante";
+            this.TextAuthor.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.TextAuthor_LinkClicked);
+            // 
+            // ButtonClose
+            // 
+            this.ButtonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.ButtonClose.Location = new System.Drawing.Point(525, 127);
+            this.ButtonClose.Name = "ButtonClose";
+            this.ButtonClose.Size = new System.Drawing.Size(75, 23);
+            this.ButtonClose.TabIndex = 0;
+            this.ButtonClose.Text = "閉じる";
+            this.ButtonClose.UseVisualStyleBackColor = true;
+            this.ButtonClose.Click += new System.EventHandler(this.ButtonClose_Click);
+            // 
+            // TextInformation
+            // 
+            this.TextInformation.AutoSize = true;
+            this.TextInformation.Location = new System.Drawing.Point(12, 72);
+            this.TextInformation.Name = "TextInformation";
+            this.TextInformation.Size = new System.Drawing.Size(235, 15);
+            this.TextInformation.TabIndex = 5;
+            this.TextInformation.TabStop = true;
+            this.TextInformation.Text = "http://electronicobserver.blog.fc2.com/";
+            this.TextInformation.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.TextInformation_LinkClicked);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Margin = new System.Windows.Forms.Padding(3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 15);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "配布元(不具合報告・連絡はこちらへ)：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 101);
+            this.label3.Margin = new System.Windows.Forms.Padding(3);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "魔改造：";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(73, 101);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(101, 15);
+            this.linkLabel1.TabIndex = 7;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Mochizuki_Mk19";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // DialogVersion
+            // 
+            this.AcceptButton = this.ButtonClose;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(612, 162);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TextInformation);
+            this.Controls.Add(this.ButtonClose);
+            this.Controls.Add(this.TextAuthor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TextVersion);
+            this.Font = new System.Drawing.Font("Meiryo UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "DialogVersion";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "バージョン";
+            this.Load += new System.EventHandler(this.DialogVersion_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
@@ -133,5 +159,7 @@
 		private System.Windows.Forms.Button ButtonClose;
 		private System.Windows.Forms.LinkLabel TextInformation;
 		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.LinkLabel linkLabel1;
 	}
 }
