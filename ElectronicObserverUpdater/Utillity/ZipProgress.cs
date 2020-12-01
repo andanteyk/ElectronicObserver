da0c97f08716aa8480fd306a7c304a8bb52a9cbf
Mochizuki_Mk19 <Tsukumoyarei@gmail.com>
2020-12-01 20:43:16 +0100

    autoupdater

---
+++ ElectronicObserverUpdater/Utillity/ZipProgress.cs
@@ -0,0 +1,34 @@
+﻿namespace ElectronicObserverUpdater
+{
+	/// <summary>
+	/// This indicates extracting progress of zip file.
+	/// </summary>
+	public class ZipProgress
+	{
+		/// <summary>
+		/// Init ZipProgress object
+		/// </summary>
+		/// <param name="total">default total value</param>
+		/// <param name="processed">default progress value</param>
+		/// <param name="currentItem">currently working item</param>
+		public ZipProgress(int total, int processed, string currentItem)
+		{
+			Total = total;
+			Processed = processed;
+			CurrentItem = currentItem;
+		}
+		/// <summary>
+		/// Total item count of zip file.
+		/// </summary>
+		public int Total { get; }
+		/// <summary>
+		/// Extracted item count of zip file.
+		/// </summary>
+		public int Processed { get; }
+		/// <summary>
+		/// The filename of currently extracting.
+		/// </summary>
+		public string CurrentItem { get; }
+	}
+
+}
